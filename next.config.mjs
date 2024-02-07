a1f8ef1cbc1e4c7c1e94ff054c77c737a286433d
Alex Rubanau <a.rubanau@flatlogic.com>
2024-02-07 14:12:23 +0300

    Add output, basePath, and unoptimized images settings in nextConfig.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/next-countries-app",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
