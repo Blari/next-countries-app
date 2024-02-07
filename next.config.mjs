3053b380a1f47851122a61e7be51d36226c1d0ef
Alex Rubanau <a.rubanau@flatlogic.com>
2024-02-07 14:29:32 +0300

    Add 'distDir' property to next.config.mjs configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  distDir: "build",
-
   /**
    * Set base path. This is the slug of your GitHub repository.
    *
