5f60ee0bc6cd1558a79bd1e51db2be5872d9d2c8
Alex Rubanau <a.rubanau@flatlogic.com>
2024-02-07 14:44:58 +0300

    Remove basePath from Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
